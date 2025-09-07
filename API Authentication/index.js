6f2d94f74a6d06fbb7f8b7a0e5941983a8bbab02
Potuzhnyj <197530600+potuzhnyj@users.noreply.github.com>
2025-09-07 12:40:47 +0000

    API Auth

---
+++ API Authentication/index.js
@@ -0,0 +1,86 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com/";
+
+//TODO 1: Fill in your values for the 3 types of auth.
+const yourUsername = "FreshOgurec";
+const yourPassword = "abc123";
+const yourAPIKey = "1ecf7458-107c-4878-a6b1-a958eef20cb3";
+const yourBearerToken = "a8e4b57a-7843-47b1-a3e1-d11952baec03";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}random`);
+    const result = JSON.stringify(response.data.secret);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}all?page=2`, {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    const result = JSON.stringify(response.data);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.get("/apiKey", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}filter`, {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    const result = JSON.stringify(response.data);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.get("/bearerToken", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}secrets/42`, {
+      headers: {
+        Authorization: `Bearer ${yourBearerToken}`,
+      },
+    });
+    const result = JSON.stringify(response.data.secret);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
